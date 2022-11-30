73d160155c763476da5d4ad6655a1fc9be78a62c
pauline-shen <s9633291@gmail.com>
2022-11-30 16:56:56 -0500

    add style and basics of appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classNames from "classnames";
+
+import "components/Appointment/styles.scss"
+
+export default function Appointment(props) {
+
+  
+
+  return (
+    <article className="appointment"></article>
+  );
+}
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -10,13 +10,13 @@ export default function InterviewerListItem(props) {
   });
 
   return (
-    <li className={interviewerClass} onClick={() => props.setInterviewer}>
+    <li className={interviewerClass} onClick={props.setInterviewer} >
       <img
         className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
-      {props.selected && <>{props.name}</>}
+      {props.selected && props.name}
     </li>
   );
 }
