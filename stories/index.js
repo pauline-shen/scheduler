9b5d5f23fb53f1c6d7129e6e3eac2983b6b7ab85
pauline-shen <s9633291@gmail.com>
2022-11-30 17:45:21 -0500

    add error and status componenet for appt

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,8 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 import "index.scss";
 
@@ -164,6 +166,17 @@ storiesOf("Appointment", module)
       message="Delete the appointment?"
       onConfirm={action("onConfirm")}
       onCancel={action("onCancel")}
+    />
+  ))
+  .add("Status", () => (
+    <Status
+      message="Deleting"
+    />
+  ))
+  .add("Error", () => (
+    <Error
+      message="Could not delete appointment."
+      onClose={action("onClose")}
     />
   ));
 
