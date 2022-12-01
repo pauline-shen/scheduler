0e2797f73e4f74bab735e094191eedd886e5651d
pauline-shen <s9633291@gmail.com>
2022-12-01 00:52:32 -0500

    add display interview in appointment componenet

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,20 @@
 import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 import "components/Appointment/styles.scss"
 
 export default function Appointment(props) {
 
-  
-
   return (
-    <article className="appointment">{props.time}</article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ?
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+        /> : <Empty />}
+    </article>
   );
 }
