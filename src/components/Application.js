0638163b47495e9e7cdc4835fa791312489b817c
pauline-shen <s9633291@gmail.com>
2022-12-01 01:07:44 -0500

    last appointment for the day

--- src/components/Application.js
+++ src/components/Application.js
@@ -93,6 +93,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {appointmentList}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
