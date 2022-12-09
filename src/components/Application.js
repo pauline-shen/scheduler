d4a58ba65cfe91d6120baab5eaf75cc9b52ea383
pauline-shen <s9633291@gmail.com>
2022-12-08 23:04:21 -0500

    change all states to a single state object

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,19 +45,25 @@ const appointments = {
   }
 };
 
+
 export default function Application(props) {
 
-  const [appointments, setAppointments] = useState({})
+  //const [appointments, setAppointments] = useState({})
   const [state, setState] = useState({
     day: "Monday",
     days: [],
   });
 
+  const setDay = day => setState({ ...state, day });
+
+  const setDays = (days) => {
+    setState(prev => ({ ...prev, days }));
+  }
+
+
   useEffect(() => {
-    axios.get('/api/days').then(response => {
-      setDays([...response.data]);
-    })
-}, [])
+    axios.get('/api/days').then(response => setDays(response.data));
+  }, [])
 
   const appointmentList = Object.values(appointments).map((appointment) =>
     <Appointment
@@ -76,7 +82,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList days={state.days} value={state.day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
