63080b951cb08d339a0d291e3549f3c80f1d482f
pauline-shen <s9633291@gmail.com>
2022-12-10 01:36:58 -0500

    build transition func for tracking current mode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+
+  function transition(newMode) {
+    setMode(newMode);
+  }
+
+  return { mode, transition };
+}
+
